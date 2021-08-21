7ca5d115960ab09c7afe24443b4b13e3e587d511
Ferdian Julianto <Juliantoferdian24@gmail.com>
2021-08-21 15:16:21 +0700

    Retrofit setup

---
+++ app/src/main/java/com/ferdian/newsappmvvm/Source.kt
@@ -0,0 +1,6 @@
+package com.ferdian.newsappmvvm
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/ferdian/newsappmvvm/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.ferdian.newsappmvvm.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "7ab3ff154e9146da825efe58c9f4023c"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
