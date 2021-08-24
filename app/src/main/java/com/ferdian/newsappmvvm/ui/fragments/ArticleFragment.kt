949f18dc51f1966793982aeff0b21c2f8c56dfd3
Ferdian Julianto <Juliantoferdian24@gmail.com>
2021-08-24 13:20:12 +0700

    Open Article on WebView

--- app/src/main/java/com/ferdian/newsappmvvm/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/ferdian/newsappmvvm/ui/fragments/ArticleFragment.kt
@@ -2,16 +2,35 @@ package com.ferdian.newsappmvvm.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.ferdian.newsappmvvm.R
+import com.ferdian.newsappmvvm.databinding.FragmentArticleBinding
+import com.ferdian.newsappmvvm.databinding.FragmentSearchNewsBinding
 import com.ferdian.newsappmvvm.ui.NewsActivity
 import com.ferdian.newsappmvvm.ui.NewsViewModel
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
     lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+
+    val TAG = "ArticleFragment"
+    private var _binding: FragmentArticleBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentArticleBinding.bind(view)
+
         viewModel = (activity as NewsActivity).viewModel
+
+        val article = args.article
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+
+
     }
 }
--- app/src/main/java/com/ferdian/newsappmvvm/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/ferdian/newsappmvvm/ui/fragments/SavedNewsFragment.kt
@@ -3,15 +3,46 @@ package com.ferdian.newsappmvvm.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ferdian.newsappmvvm.R
+import com.ferdian.newsappmvvm.adapters.NewsAdapter
+import com.ferdian.newsappmvvm.databinding.FragmentBreakingNewsBinding
+import com.ferdian.newsappmvvm.databinding.FragmentSavedNewsBinding
 import com.ferdian.newsappmvvm.ui.NewsActivity
 import com.ferdian.newsappmvvm.ui.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SavedNewsFragment"
+    private var _binding: FragmentSavedNewsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSavedNewsBinding.bind(view)
+
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
